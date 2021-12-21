8e861a32f9003558ccf72a42f12c71ead5d19a89
DESKTOP-ESA8F4M\Jordan <jordan.t.donovancs@gmail.com>
2021-12-20 23:00:29 -0500

    starting with evolutionary algorithm

---
+++ randomdataset.py
@@ -0,0 +1,20 @@
+import torch
+import cv2
+class RandomDataset(torch.utils.data.Dataset):
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        label = 'random'
+        if self.transform is not None:
+            image = self.transform(image=image)['image']
+
+        return image, label
