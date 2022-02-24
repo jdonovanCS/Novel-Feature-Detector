c15fd5180efe7fa525b49ad56b3352fce6c355bb
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-02-24 13:11:15 -0500

    typo

--- evolution.py
+++ evolution.py
@@ -31,7 +31,7 @@ def compute_feature_novelty(activations):
                 if str(i) not in dist:
                     dist[str(i)] = []
                 dist[str(i)].append(np.abs(a2 - a))
-        avg_dist[str(i)] = np.mean(dist[str(i)]).numpy()
+        avg_dist[str(i)] = np.mean(dist[str(i)])
     return(sum(avg_dist.values()))
 
 def mutate(filters):
