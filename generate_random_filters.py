9147ff6d3f56b5ad5d17f5ae449e43ad1ec6f2ae
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-03-31 12:54:12 -0400

    fixing some errors helper_hpc, train_and_eval, generate_random_filters. Moving network class to its own file. Adding classes to the logging of network

--- generate_random_filters.py
+++ generate_random_filters.py
@@ -6,38 +6,43 @@ import pickle
 import numpy as np
 import torch
 import argparse
-import time
 
 parser=argparse.ArgumentParser(description="Process some input files")
 parser.add_argument('--dataset', help='which dataset should be used for novelty metric, choices are: random, cifar-10', default='random')
 parser.add_argument('--experiment_name', help='experiment name for saving and data related to filters generated', default='')
 parser.add_argument('--population_size', help='number of filters to generate', type=int, default=50)
+parser.add_argument('--batch_size', help='Number of images to use for novelty metric, only 1 batch used', default=64, type=int)
 args = parser.parse_args()
 
 def run():
     torch.multiprocessing.freeze_support()
-    helper.run()
+    helper.run(seed=False)
     
     global trainloader
-    if args.dataset.lower() != 'cifar-10':
+    if args.dataset.lower() != 'cifar-10' and args.dataset.lower() != 'cifar10':
         random_image_paths = helper.create_random_images(64)
-        trainloader = helper.load_random_images(random_image_paths)
+        data_module = helper.load_random_images(random_image_paths)
     else:
-        trainloader = helper.load_CIFAR_10()[2]
-    
-    global experiment_name
+        data_module = helper.get_data_module(args.dataset.lower(), batch_size=args.batch_size)
     
     experiment_name = args.experiment_name    
     population_size = args.population_size
     population = []
 
+    data_module.prepare_data(data_dir="data/")
+    data_module.setup()
+    classnames = list(data_module.dataset_test.classes)
+
+    data_iterator = iter(data_module.train_dataloader())
+    net_input = next(data_iterator)
+
     for i in tqdm(range(population_size)): #while len(population) < population_size:
         model = evol.Model()
-        model.filters = helper.get_random_filters()
-        # TODO: The below funciton is far too slow
-        model.activations = helper.get_activations(trainloader, model.filters)
-        model.fitness = evol.compute_feature_novelty(model.activations)
+        net = helper.Net(num_classes=len(classnames), classnames=classnames)
+        model.filters = net.get_filters()
+        model.fitness =  net.get_fitness(net_input)
         population.append(model)
+        helper.wandb.log({'gen': 0, 'individual': i, 'fitness': model.fitness})
         
     sols = [p.filters for p in population]
     solutions = np.array([[evol.Model() for i in range(population_size)]for j in range(1)], dtype=object)
