aa30c0d62f90b332d619ed64468fbee0f1801d20
Jordan Donovan <47909015+jdonovanCS@users.noreply.github.com>
2023-09-06 09:01:50 -0500

    evolution using training data and shuffle as well as other minor changes

--- extract_imagenet.py
+++ extract_imagenet.py
@@ -2,10 +2,15 @@ import tarfile
 import os
 
 print(os.listdir('data/imagenet/train'))
-
-for f in [x for x in os.listdir('./data/imagenet/train/') if ('.tar' in x)]:
+count=0
+for f in [x for x in os.listdir('./data/imagenet/train/') if os.path.isdir('./data/imagenet/train/'+x)]:
     print(f)
-    my_tar = tarfile.open('data/imagenet/train/{}'.format(f))
-    my_tar.extractall('data/imagenet/train/{}'.format(f.split('.')[0]))
-    my_tar.close()
+    for f2 in os.listdir('data/imagenet/train/{}'.format(f)):
+        count+=1
+
+print(count)
+
+    # my_tar = tarfile.open('data/imagenet/val/{}'.format(f))
+    # my_tar.extractall('data/imagenet/val/{}'.format(f.split('.')[0]))
+    # my_tar.close()
 
