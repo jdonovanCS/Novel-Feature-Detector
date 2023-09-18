bb3890b36ef4a462a0d3e664f537b3d2ba64803d
jdonovanCS <jordan.t.donovancs@gmail.com>
2023-09-18 10:07:30 -0400

    testing pytorch built in vgg16

--- vgg16.py
+++ vgg16.py
@@ -8,6 +8,7 @@ import helper_hpc as helper
 import time
 import torchmetrics
 import torchvision.transforms as transform
+import torchvision.models as models
 
 # DEFINE a CONV NN
 
@@ -15,40 +16,43 @@ class Net(pl.LightningModule):
     def __init__(self, num_classes=10, classnames=None, diversity=None, lr=.1):
         super().__init__()
 
+        #manual optimization - TAKE OUT LATER
+        # self.automatic_optimization = False
+
         self.save_hyperparameters()
 
-        self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(25088, 4096)
-        self.fc2 = nn.Linear(4096, 4096)
-        self.fc3 = nn.Linear(4096, num_classes)
+        # self.pool = nn.MaxPool2d(2,2)
+        # self.fc1 = nn.Linear(25088, 4096)
+        # self.fc2 = nn.Linear(4096, 4096)
+        # self.fc3 = nn.Linear(4096, num_classes)
 
-        self.conv_layers = nn.ModuleList([nn.Conv2d(3, 64, 3,padding=1),
-                                          nn.Conv2d(64, 64, 3, padding=1),
-                                          nn.Conv2d(64, 128, 3, padding=1),
-                                          nn.Conv2d(128, 128, 3, padding=1),
-                                          nn.Conv2d(128, 256, 3, padding=1),
-                                          nn.Conv2d(256, 256, 3, padding=1),
-                                          nn.Conv2d(256, 256, 3, padding=1),
-                                          nn.Conv2d(256, 512, 3, padding=1),
-                                          nn.Conv2d(512, 512, 3, padding=1),
-                                          nn.Conv2d(512, 512, 3, padding=1),
-                                          nn.Conv2d(512, 512, 3, padding=1),
-                                          nn.Conv2d(512, 512, 3, padding=1),
-                                          nn.Conv2d(512, 512, 3, padding=1)])
+        # self.conv_layers = nn.ModuleList([nn.Conv2d(3, 64, 3,padding=1),
+        #                                   nn.Conv2d(64, 64, 3, padding=1),
+        #                                   nn.Conv2d(64, 128, 3, padding=1),
+        #                                   nn.Conv2d(128, 128, 3, padding=1),
+        #                                   nn.Conv2d(128, 256, 3, padding=1),
+        #                                   nn.Conv2d(256, 256, 3, padding=1),
+        #                                   nn.Conv2d(256, 256, 3, padding=1),
+        #                                   nn.Conv2d(256, 512, 3, padding=1),
+        #                                   nn.Conv2d(512, 512, 3, padding=1),
+        #                                   nn.Conv2d(512, 512, 3, padding=1),
+        #                                   nn.Conv2d(512, 512, 3, padding=1),
+        #                                   nn.Conv2d(512, 512, 3, padding=1),
+        #                                   nn.Conv2d(512, 512, 3, padding=1)])
         
-        self.BatchNorm64_1 = nn.BatchNorm2d(64)
-        self.BatchNorm64_2 = nn.BatchNorm2d(64)
-        self.BatchNorm128_1 = nn.BatchNorm2d(128)
-        self.BatchNorm128_2 = nn.BatchNorm2d(128)
-        self.BatchNorm256_1 = nn.BatchNorm2d(256)
-        self.BatchNorm256_2 = nn.BatchNorm2d(256)
-        self.BatchNorm256_3 = nn.BatchNorm2d(256)
-        self.BatchNorm512_1 = nn.BatchNorm2d(512)
-        self.BatchNorm512_2 = nn.BatchNorm2d(512)
-        self.BatchNorm512_3 = nn.BatchNorm2d(512)
-        self.BatchNorm512_4 = nn.BatchNorm2d(512)
-        self.BatchNorm512_5 = nn.BatchNorm2d(512)
-        self.BatchNorm512_6 = nn.BatchNorm2d(512)
+        # self.BatchNorm64_1 = nn.BatchNorm2d(64)
+        # self.BatchNorm64_2 = nn.BatchNorm2d(64)
+        # self.BatchNorm128_1 = nn.BatchNorm2d(128)
+        # self.BatchNorm128_2 = nn.BatchNorm2d(128)
+        # self.BatchNorm256_1 = nn.BatchNorm2d(256)
+        # self.BatchNorm256_2 = nn.BatchNorm2d(256)
+        # self.BatchNorm256_3 = nn.BatchNorm2d(256)
+        # self.BatchNorm512_1 = nn.BatchNorm2d(512)
+        # self.BatchNorm512_2 = nn.BatchNorm2d(512)
+        # self.BatchNorm512_3 = nn.BatchNorm2d(512)
+        # self.BatchNorm512_4 = nn.BatchNorm2d(512)
+        # self.BatchNorm512_5 = nn.BatchNorm2d(512)
+        # self.BatchNorm512_6 = nn.BatchNorm2d(512)
         
         self.activations = {}
         for i in range(len(self.conv_layers)):
@@ -62,100 +66,103 @@ class Net(pl.LightningModule):
         # self.avg_novelty = 0
 
     def forward(self, x, get_activations=False):
-        if x.size(dim=2) == 32:
-            tr = transform.Resize((224,224))
-            x = tr(x)
-        conv_count = 0
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm64_1(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm64_2(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm128_1(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm128_2(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm256_1(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm256_2(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm256_3(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm512_1(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm512_2(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm512_3(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm512_4(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm512_5(x)
-        x = F.relu(x)
-        x = self.conv_layers[conv_count](x)
-        if get_activations:
-            self.activations[conv_count].append(x)
-        conv_count += 1
-        x = self.BatchNorm512_6(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
-        return x
+        # if x.size(dim=2) == 32:
+        #     tr = transform.Resize((224,224))
+        #     x = tr(x)
+        # conv_count = 0
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm64_1(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm64_2(x)
+        # x = F.relu(x)
+        # x = self.pool(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm128_1(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm128_2(x)
+        # x = F.relu(x)
+        # x = self.pool(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm256_1(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm256_2(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm256_3(x)
+        # x = F.relu(x)
+        # x = self.pool(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm512_1(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm512_2(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm512_3(x)
+        # x = F.relu(x)
+        # x = self.pool(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm512_4(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm512_5(x)
+        # x = F.relu(x)
+        # x = self.conv_layers[conv_count](x)
+        # if get_activations:
+        #     self.activations[conv_count].append(x)
+        # conv_count += 1
+        # x = self.BatchNorm512_6(x)
+        # x = F.relu(x)
+        # x = self.pool(x)
+        # x = torch.flatten(x, 1)
+        # x = self.fc1(x)
+        # x = F.relu(x)
+        # x = self.fc2(x)
+        # x = F.relu(x)
+        # x = self.fc3(x)
+        # return x
+
+        op = models.vgg16(pretrained=False)
+        return op(x)
 
         output = F.log_softmax(x, dim=1)
         return output
