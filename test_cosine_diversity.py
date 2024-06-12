c37cf167977639b8e5e07f439666d4362ba6f9ea
Jordan Donovan <47909015+jdonovanCS@users.noreply.github.com>
2024-06-12 16:05:08 -0500

    test cosine diversity stuff

--- test_cosine_diversity.py
+++ test_cosine_diversity.py
@@ -70,36 +70,55 @@ print((e_filters[0][0][0][0][0]))
 print((e_filters[0][0][0][0][1]))
 print()
 
+# For each run
 for i in range(len(e_filters)):
     # print(helper.cosine_dist(np.array(e_filters[i][49][0][0][0]).flatten(), np.array(e_filters[i][49][0][0][1]).flatten()))
     # print(helper.cosine_dist(np.array(e_filters[i][6][0][0][0]).flatten(), np.array(e_filters[i][6][0][0][1]).flatten()))
-    for j in range(len(e_filters[i])):
-
+    # For each generation
+    num_diffs = 0
+    for j in range(0, len(e_filters[i]), len(e_filters[i])):
+        
+        # For each layer
         for k in range(len(e_filters[i][j])):
-            # sum_of_ee = torch.sum(torch.eq(e_filters[i][j][k], e_filters[i][4][j]))
-            sum_of_ei = torch.sum(torch.eq(e_filters[i][j][k], i_filters[i][j][k]))
-            # sum_of_ii = torch.sum(torch.eq(i_filters[i][j][k], i_filters[i][4][j]))
-            len_of_filters = len(e_filters[i][j][k].flatten())
-            # assert(sum_of_ee == len_of_filters)
-            # print('run:', i, 'gen:', j, 'layer:', k, sum_of_ei == len_of_filters)
-            # assert(sum_of_ii == len_of_filters)
+            if torch.sum(i_filters[i][j][k] == i_filters[i][49][k]) != 0:
+                num_diffs += int(torch.sum(i_filters[i][j][k] != i_filters[i][49][k]))/9
+                #    - len(i_filters[i][j][k].flatten()))
+                # sum_of_ee = torch.sum(torch.eq(e_filters[i][j][k], e_filters[i][4][j]))
+                # sum_of_ei = torch.sum(torch.eq(e_filters[i][j][k], i_filters[i][j][k]))
+                # sum_of_ii = torch.sum(i_filters[i][j][k] == i_filters[i][49][k])
+                # len_of_filters = len(i_filters[i][j][k].flatten())
+                # assert(sum_of_ee == len_of_filters)
+                # print('run:', i, 'gen:', j, 'layer:', k, sum_of_ii == len_of_filters)
+        #       # assert(sum_of_ii == len_of_filters)
+
+        #     # For each filter
+        #     # print(diff)
+        #     for l in range(len(e_filters[i][j][k])):
+                
+    #             for m in range(len(e_filters[i][j][k][l])):
+    #                 diff=diff+1 if torch.sum(torch.eq(i_filters[i][j][k][l], i_filters[i][49][k][l])) != len(i_filters[i][j][k][l].flatten()) else diff
+    #                 count+=1
+
+    #                 for n in range(len(e_filters[i][j][k][l][m])):
+    #                     diff2=diff2+1 if torch.sum(torch.eq(i_filters[i][j][k][l][m], i_filters[i][49][k][l][m])) != len(i_filters[i][j][k][l][m].flatten()) else diff2
+    #                     count2+=1
+    print(num_diffs)
 
 
-
-    # helper.run(seed=False)
-    data_module = helper.get_data_module("random", batch_size=64, workers=0)
-    data_module.prepare_data()
-    data_module.setup()
-    trainer = pl.Trainer(accelerator="auto", limit_val_batches=1)
-    trainer2 = pl.Trainer(accelerator="auto", limit_val_batches=1)
-    classnames = list(data_module.dataset_test.classes)
-    net = helper.Net(num_classes=len(classnames), classnames=classnames, diversity={"type": 'cosine', "pdop": 'mean', "ldop":'w_mean', 'k': -1, 'k_strat': 'closest'})
-    net2 = helper.Net(num_classes=len(classnames), classnames=classnames, diversity={"type": 'cosine', "pdop": 'mean', "ldop":'w_mean', 'k': -1, 'k_strat': 'closest'})
-    net.set_filters(i_filters[i][10])
-    net2.set_filters(i_filters[i][49])
-    trainer.validate(net, dataloaders=data_module.val_dataloader(), verbose=False)
-    trainer2.validate(net2, dataloaders=data_module.val_dataloader(), verbose=False)
-    print(net.avg_novelty, net2.avg_novelty)
+    # # helper.run(seed=False)
+    # data_module = helper.get_data_module("random", batch_size=64, workers=0)
+    # data_module.prepare_data()
+    # data_module.setup()
+    # trainer = pl.Trainer(accelerator="auto", limit_val_batches=1)
+    # trainer2 = pl.Trainer(accelerator="auto", limit_val_batches=1)
+    # classnames = list(data_module.dataset_test.classes)
+    # net = helper.Net(num_classes=len(classnames), classnames=classnames, diversity={"type": 'cosine', "pdop": 'mean', "ldop":'w_mean', 'k': -1, 'k_strat': 'closest'})
+    # net2 = helper.Net(num_classes=len(classnames), classnames=classnames, diversity={"type": 'cosine', "pdop": 'mean', "ldop":'w_mean', 'k': -1, 'k_strat': 'closest'})
+    # net.set_filters(i_filters[i][10])
+    # net2.set_filters(i_filters[i][49])
+    # trainer.validate(net, dataloaders=data_module.val_dataloader(), verbose=False)
+    # trainer2.validate(net2, dataloaders=data_module.val_dataloader(), verbose=False)
+    # print(net.avg_novelty, net2.avg_novelty)
         
 
 
